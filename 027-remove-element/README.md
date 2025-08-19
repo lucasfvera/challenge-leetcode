b39276abb305a5a07ad9267871e5e7cf2f0ac6ef
lucasfvera <lucasfvera@hotmail.com>
2025-08-18 22:31:24 -0300

    027: add two pointers approach

--- 027-remove-element/README.md
+++ 027-remove-element/README.md
@@ -35,6 +35,8 @@ Input: nums = [3,2,2,3], val = 3
 Output: 2, nums = [2,2,_,_]
 Explanation: Your function should return k = 2, with the first two elements of nums being 2.
 It does not matter what you leave beyond the returned k (hence they are underscores).
+
+
 Example 2:
 
 Input: nums = [0,1,2,2,3,0,4,2], val = 2
--- 027-remove-element/index.ts
+++ 027-remove-element/index.ts
@@ -1,17 +1,31 @@
-// Brute force
+// Brute force -> O(n^2)
 function removeElement(nums: number[], val: number): number {
     let aux = [];
     for (let i = 0; i < nums.length; i++) {
+        // O(n)
         if (nums[i] !== val) aux.push(nums[i]);
     }
-    nums.splice(0, aux.length, ...aux);
+    nums.splice(0, aux.length, ...aux); // O(n)
 
     return aux.length;
+}
+
+function removeElementTwoPointers(nums: number[], val: number): number {
+    let target = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[target] = nums[i];
+            target++;
+        }
+    }
+    return target;
 }
 
 // Input: nums = [3,2,2,3], val = 3
 // Output: 2, nums = [2,2,_,_]
 
-const result = removeElement([3, 2, 2, 3], 3);
+const ex = [3, 2, 2, 3, 4, 3, 1, 5];
+const result = removeElementTwoPointers(ex, 3);
 
+console.log(ex);
 console.log(result);
