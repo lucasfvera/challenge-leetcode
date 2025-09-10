f3220ea2809ac80fa1c5918705daa88006f5ba28
lucasfvera <lucasferreyravera@gmail.com>
2025-09-09 21:14:28 -0300

    283: Add O(n^2) and O(n) solutions

---
+++ 283-move-zeroes/index.js
@@ -0,0 +1,35 @@
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function moveZeroes(nums) {
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === 0) {
+            for (let j = i + 1; j < nums.length; j++) {
+                if (nums[j] !== 0) {
+                    nums[i] = nums[j];
+                    nums[j] = 0;
+                    break;
+                }
+            }
+        }
+    }
+}
+;
+function moveZeroesOptimized(nums) {
+    let zeroPosition;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === 0 && zeroPosition === undefined) {
+            zeroPosition = i;
+        }
+        else if (nums[i] !== 0 && zeroPosition !== undefined) {
+            nums[zeroPosition] = nums[i];
+            nums[i] = 0;
+            zeroPosition++;
+        }
+    }
+}
+;
+// const nums = [0, 1, 0, 3, 12]
+const nums = [0];
+moveZeroesOptimized(nums);
+console.log(nums);
